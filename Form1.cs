acab1320d1ab1b4375675aa6918d349605122d54
Jake <jake.monte@tuta.io>
2019-05-20 23:15:06 -0400

    Clean Code

--- Form1.cs
+++ Form1.cs
@@ -17,7 +17,6 @@ namespace Varo
     public partial class Form1 : DevExpress.XtraEditors.XtraForm
     {
         IXboxConsole xbox; //Define Console Variable
-
         public Form1(){
             InitializeComponent();
         }
@@ -25,25 +24,24 @@ namespace Varo
         private void Form1_Load(object sender, EventArgs e){
             SkinHelper.InitSkinPopupMenu(SkinsLink);
         }
-        private void SimpleButton1_Click(object sender, EventArgs e)
-        {
+        private void SimpleButton1_Click(object sender, EventArgs e){
             //Connecting Xbox 
-            if (xbox.Connect(out xbox)){ 
-                MessageBox.Show("Successfully Connected to Console!");
-                xbox.XNotify("Connected to Varo!");
-                xbox.XNotify("Varo, Created by Jake! <3");
-                xbox.XNotify("Open-Sourced on GitHub!");
-                label1.ForeColor = Color.Green;
-                label1.Text = "Status: Connected!";
-                textBox1.Text = ("Kernel Version: " + xbox.GetKernalVersion());
-                textBox2.Text = ("CPU Key: " + xbox.GetCPUKey());
-                textBox3.Text = ("CPU Temperature: " + xbox.GetTemperature(JRPC.TemperatureType.CPU));
-                textBox4.Text = ("GPU Temperature: " + xbox.GetTemperature(JRPC.TemperatureType.GPU));
-                textBox5.Text = ("Motherboard Temperature: " + xbox.GetTemperature(JRPC.TemperatureType.MotherBoard));
-                textBox6.Text = ("IP Address: " + xbox.XboxIP());
-            }else{
-                MessageBox.Show("Connect Locate Console!"); //This can be due to Neighborhood not installed or JRPC2 as plugin. 
-            }
+         if (xbox.Connect(out xbox)){ 
+            MessageBox.Show("Successfully Connected to Console!");
+            xbox.XNotify("Connected to Varo!");
+            xbox.XNotify("Varo, Created by Jake! <3");
+            xbox.XNotify("Open-Sourced on GitHub!");
+            label1.ForeColor = Color.Green;
+            label1.Text = "Status: Connected!";
+            textBox1.Text = ("Kernel Version: " + xbox.GetKernalVersion());
+            textBox2.Text = ("CPU Key: " + xbox.GetCPUKey());
+            textBox3.Text = ("CPU Temperature: " + xbox.GetTemperature(JRPC.TemperatureType.CPU));
+            textBox4.Text = ("GPU Temperature: " + xbox.GetTemperature(JRPC.TemperatureType.GPU));
+            textBox5.Text = ("Motherboard Temperature: " + xbox.GetTemperature(JRPC.TemperatureType.MotherBoard));
+            textBox6.Text = ("IP Address: " + xbox.XboxIP());
+        }else{
+            MessageBox.Show("Connect Locate Console!"); //This can be due to Neighborhood not installed or JRPC2 as plugin. 
+        }
         }
     }
 }
